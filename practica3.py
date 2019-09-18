e8634720fcd758486162abebedf7109d874e9bfa
Victor Flores <vflorespineda@gmail.com>
2019-09-18 04:30:04 -0500

    Práctica convolución

--- practica3.py
+++ practica3.py
@@ -11,7 +11,7 @@ y = np.convolve(x,h)
 plt.stem(np.linspace(0,6,7),y)
 
 
-#Ejercicio 2
+#Ejercicio 2 propiedad conmutativa
 plt.figure()
 n = np.linspace(0,20,21)
 x = np.cos((np.pi/10)*n)
@@ -29,7 +29,7 @@ y2 = np.convolve(h[0],x)
 plt.subplot(224)
 plt.stem(np.linspace(0,30,31),y2)
 
-#Ejercicio 3
+#Ejercicio 3 propiedad distributiva
 plt.figure()
 h1 = h[0]
 h2 = -1*np.random.rand(1,11)[0]
@@ -63,4 +63,35 @@ y4 = np.convolve(x,h1)+np.convolve(x,h2)
 plt.subplot(338)
 plt.stem(x3,y4)
 
+#Ejercicio 4 propiedad asociativa
+plt.figure()
+plt.subplot(331)
+plt.stem(n,x)
+plt.subplot(332)
+plt.stem(x2,h1)
+plt.subplot(333)
+plt.stem(x2,h2)
+
+y1 = np.convolve(x,h1)
+plt.subplot(334)
+plt.stem(x3,y1)
+
+y2 = np.convolve(h2,h1)
+x3 = np.linspace(0,20,21)
+plt.subplot(335)
+plt.stem(x3,y2)
+
+x4 = np.linspace(0,40,41)
+
+y3 = np.convolve(y1,h2)
+plt.subplot(337)
+plt.stem(x4,y3)
+
+y4 = np.convolve(x,y2)
+plt.subplot(338)
+plt.stem(x4,y4)
+
+
+
 plt.show()
+
