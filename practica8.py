70d1d13d14e7f51309817710e9939b2912468a37
Victor Flores <vflorespineda@gmail.com>
2019-11-04 16:47:28 -0600

    Solo falta diseño

--- practica8.py
+++ practica8.py
@@ -16,38 +16,107 @@ print(y)
 #A
 num = [1,-3]
 den = [1,4,4]
-h1 = signal.TransferFunction(num, den)
+h1 = control.TransferFunction(num,den)
 [zeros,poles,gain] = signal.tf2zpk(num,den)
+print("h1")
+print("zeros")
 print(zeros)
+print("polos ")
 print(poles)
-print(gain)
+print("\n")
+control.pzmap(h1, Plot=True, grid=False, title='H1')
+[t,y] = signal.impulse( ([1,-3],[1,4,4]) )
+plt.plot(t, y)
+[t,y] = signal.step( ([1,-3],[1,4,4]) )
+plt.plot(t, y)
 
 #B
 num = [1]
 den = [1,4,4]
-h2 = signal.TransferFunction(num, den)
+h2 = control.TransferFunction(num, den)
 [zeros,poles,gain] = signal.tf2zpk(num,den)
+print("h2")
+print("zeros")
+print(zeros)
+print("polos ")
+print(poles)
+print("\n")
+control.pzmap(h2, Plot=True, grid=False, title='H2')
+[t,y] = signal.impulse( ([1],[1,4,4]) )
+plt.plot(t, y)
+[t,y] = signal.step( ([1],[1,4,4]) )
+plt.plot(t, y)
+
 
 #C
 num = [1]
 den = [1,20,100]
-h3 = signal.TransferFunction(num, den)
+h3 = control.TransferFunction(num, den)
 [zeros,poles,gain] = signal.tf2zpk(num,den)
+print("h3")
+print("zeros")
+print(zeros)
+print("polos ")
+print(poles)
+print("\n")
+control.pzmap(h3, Plot=True, grid=False, title='H3')
+[t,y] = signal.impulse( ([1],[1,20,100]) )
+plt.plot(t, y)
+[t,y] = signal.step( ([1],[1,20,100]) )
+plt.plot(t, y)
+
 
 #D
 num = [4]
 den = [1,0,1]
-h2 = signal.TransferFunction(num, den)
+h4 = control.TransferFunction(num, den)
 [zeros,poles,gain] = signal.tf2zpk(num,den)
+print("h4")
+print("zeros")
+print(zeros)
+print("polos ")
+print(poles)
+print("\n")
+control.pzmap(h4, Plot=True, grid=False, title='H4')
+[t,y] = signal.impulse( ([4],[1,0,1]) )
+plt.plot(t, y)
+[t,y] = signal.step( ([4],[1,0,1]) )
+plt.plot(t, y)
+
 
 #E
 num = [2,2]
 den = [1,1,-7,-15]
-h2 = signal.TransferFunction(num, den)
+h5 = control.TransferFunction(num, den)
 [zeros,poles,gain] = signal.tf2zpk(num,den)
+print("h5")
+print("zeros")
+print(zeros)
+print("polos ")
+print(poles)
+print("\n")
+control.pzmap(h5, Plot=True, grid=False, title='H5')
+[t,y] = signal.impulse( ([2,2],[1,1,-7,-15]) )
+plt.plot(t, y)
+[t,y] = signal.step( ([2,2],[1,1,-7,-15]) )
+plt.plot(t, y)
+
 
 #F
 num = [3,12]
 den = [1,-2,5]
-h2 = signal.TransferFunction(num, den)
+h6 = control.TransferFunction(num, den)
 [zeros,poles,gain] = signal.tf2zpk(num,den)
+print("h6")
+print("zeros")
+print(zeros)
+print("polos ")
+print(poles)
+print("\n")
+control.pzmap(h6, Plot=True, grid=False, title='H6')
+[t,y] = signal.impulse( ([3,12],[1,-2,5]) )
+plt.plot(t, y)
+[t,y] = signal.step( ([3,12],[1,-2,5]) )
+plt.plot(t, y)
+
+plt.show()
